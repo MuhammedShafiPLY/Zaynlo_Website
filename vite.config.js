04c9fbe858094bd2262485a2967fc617c5186c3d
MuhammedShafiPLY <muhammedshafiply@gmail.com>
2025-12-30 03:25:48 +0530

    Fixed React 19 conflicts and mobile lag

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
+  // 1. Force a single instance of React to prevent "Invalid Hook Call" errors
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
 })
